0fa18b037be0ad8cb841726e5334a14d0d9794ed
Jeremy Edberg <jedberg@gmail.com>
2019-04-05 12:59:31 -0700

    Add stub-functions tool

---
+++ stub-functions.py
@@ -0,0 +1,64 @@
+import ast
+import os
+import yaml
+
+
+def stub_out(func, file):
+    """This function creates the default function stubs"""
+    with open(file, "a") as myfile:
+        text = """
+def %s(event, context):
+    response = dict(statusCode=200, body="Success")
+    return response
+
+""" % (func)
+        myfile.write(text)
+
+
+# Read in the serverless.yml file and find all the functions and files that should exist
+f = ""
+with open("serverless.yml", 'r') as stream:
+    f = yaml.full_load(stream)
+
+function_handlers = [f['functions'][x]['handler'] for x in f['functions']]
+
+# Construct the file names we're looking for
+filenames = []
+for file in function_handlers:
+    filenames.append((file.split('.')[1], os.path.join(os.getcwd() + "/" +
+                                                       file.split('.')[0] +
+                                                       '.py')))
+
+# Make sure the files we need exist, and create them if they are missing
+for tup in filenames:
+    os.makedirs(os.path.dirname(tup[1]), exist_ok=True)
+    if not os.path.exists(tup[1]):
+        print("Creating "+ tup[1])
+        with open(tup[1], 'w'): pass
+
+# Make sure the functions exist in the files
+for tup in filenames:
+    with open(tup[1]) as fd:
+        file_contents = fd.read()
+    module = ast.parse(file_contents)
+    function_definitions = [node for node in module.body if isinstance(node, ast.FunctionDef)]
+    if tup[0] in [f.name for f in function_definitions]:
+        print("Found " + tup[0] + " in " + tup[1])
+    else:
+        print("Did not find " + tup[0] + " in " + tup[1] + ", stubbing out")
+        stub_out(tup[0], tup[1])
+        print("Stubbed out " + tup[0] + " in " + tup[1])
+
+# Check for extra files
+files_should_exisit = [tup[1] for tup in filenames]
+files_that_exist = []
+exclude_dirs = set([".serverless", "__pycache__", "lib"])
+exclude_files = set(["serverless.yml", "requirements.txt", "package.json", "__init__.py"])
+for root, dirs, files in os.walk(os.getcwd(), topdown=True):
+    dirs[:] = [d for d in dirs if d not in exclude_dirs]
+    files[:] = [f for f in files if f not in exclude_files]
+    for file in files:
+        files_that_exist.append((os.path.join(root, file)))
+
+for x in [x for x in files_that_exist if x not in files_should_exisit]:
+    print("File " + x + " may be unnecessary")
